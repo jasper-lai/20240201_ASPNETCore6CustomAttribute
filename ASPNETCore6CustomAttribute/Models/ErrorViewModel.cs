7786e2841b4d3fb91bb38b4ee689fc70b3c5f610
jasper.lai <jasper.lai@gmail.com>
2024-02-01 11:09:46 +0800

    加入專案檔案。

---
+++ ASPNETCore6CustomAttribute/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETCore6CustomAttribute.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
